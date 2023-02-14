d0aa5433a637a49ba1aadc27b438b873e0e9f1a1
Gary Buhrmaster <gary.buhrmaster@gmail.com>
2023-02-14 15:48:05 +0000

    clean previous testing

--- roles/buildworker/files/FreeBSD-start-buildworker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd ~/mythtv
-/usr/local/bin/buildbot-worker stop
-echo FreeBSD `/bin/freebsd-version` > info/host
-exec /usr/local/bin/buildbot-worker start
--- roles/buildworker/files/Suse-start-buildworker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ~/mythtv
-/usr/bin/buildbot-worker stop
-cat /etc/os-release > info/host
-exec /usr/bin/buildbot-worker start
-
